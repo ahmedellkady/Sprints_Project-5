4dea301500cfce68d8f75ef239f6a9131a3752f2
kadyy <ahmedkhaled12004@gmail.com>
2025-08-20 13:53:25 +0300

    Initial Project setup

---
+++ src/main/java/com/team2/university_room_booking/UniversityRoomBookingApplication.java
@@ -0,0 +1,13 @@
+package com.team2.university_room_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityRoomBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityRoomBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team2/university_room_booking/UniversityRoomBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.university_room_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityRoomBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
